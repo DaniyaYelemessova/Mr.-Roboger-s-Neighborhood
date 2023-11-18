a4c998a23324f1cf3a72f5ac550f55efa04d2c08
Daniya Yelemessova <daniya@collingsmail.com>
2023-11-17 19:27:42 -0500

    Add test to replace '1' with Beep

--- README.md
+++ README.md
@@ -3,4 +3,9 @@ Describe: beepBoop()
 Test: "It should return an array of numbers from 0 to the user's inputted number"
 Code: beepBoop(5);
 Expected Output: [0, 1, 2, 3, 4, 5]
+
+Test: "It should replace numbers that contain a "1" with "Beep"
+Code: beepBoop(1)
+Expected Output: [0,"Beep"]
+
  
--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,14 @@
 function beepBoop(number){
   let newResult = [];
   for(let i = 0; i <= number; i++){
-    newResult.push(i)
+    let stringNumber = i.toString();
+    if(stringNumber.includes("1")){
+      newResult.push("Beep")
+    }else{
+      newResult.push(i)
+    }
   }
   return newResult
 }
 
-console.log(beepBoop("5"))
+console.log(beepBoop("1"))
